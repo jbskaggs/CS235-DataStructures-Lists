f4dceb503cbd11a0a8682b7ae6fcb1472f08deb7
Jonathan Skaggs <jbskaggs12@gmail.com>
2022-06-27 11:21:29 -0600

    initial commit

--- main.cpp
+++ main.cpp
@@ -42,76 +42,76 @@ vector<string> reverse(vector<string> items) {
 
 
 int main() {
-// List Data Type (4.1, 4.6, 4.9)
+    // List Data Type (4.1, 4.6, 4.9)
 
-// What's a List?
+    // What's a List?
 
-// 	1. a container that holds objects
-// 	2. each object has an index or position
-// 	3. objects are kept in the order given by the user
+    // 	1. a container that holds objects
+    // 	2. each object has an index or position
+    // 	3. objects are kept in the order given by the user
 
-// What operations are supported by a List?
-// What's the abstract interface for a List?
+    // What operations are supported by a List?
+    // What's the abstract interface for a List?
 
-// 	size();
-// 	append(item);
-// 	remove(index);
-// 	insert(index, item);
-// 	get(index);
-// 	set(index, item);
+    // 	size();
+    // 	append(item);
+    // 	remove(index);
+    // 	insert(index, item);
+    // 	get(index);
+    // 	set(index, item);
 
-// What happens to the item at index 2 when the item at index 1 is removed?
+    // What happens to the item at index 2 when the item at index 1 is removed?
 
-// What happens to the item at index 2 when an item is inserted at index 2?
+    // What happens to the item at index 2 when an item is inserted at index 2?
 
-// Classwork
-// You may work with a partner.
+    // Classwork
+    // You may work with a partner.
 
-// What does the program print?
+    // What does the program print?
 
-//    List L1;
-//    L1.append(1);
-//    L1.append(2);
-//    L1.append(4);
-//    print(L1);
-//
-//    List L2;
-//    for (int i = 0; i < L1.size(); i++)
-//        L2.insert(0, L1.get(i));
-//    print(L2);
-//
-//    for (int i = 0; i < L1.size(); i++)
-//        L2.insert(2 * i, L1.get(i));
-//    print(L2);
-//
-//    for (int i = 0; i < L1.size(); i++)
-//        L2.set(i, L1.get(i));
-//    print(L2);
-//
-//    int len = L2.size();
-//    for (int i = 0; i < len; i++)
-//        L2.remove(i);
-//    print(L2);
+    //    List L1;
+    //    L1.append(1);
+    //    L1.append(2);
+    //    L1.append(4);
+    //    print(L1);
+    //
+    //    List L2;
+    //    for (int i = 0; i < L1.size(); i++)
+    //        L2.insert(0, L1.get(i));
+    //    print(L2);
+    //
+    //    for (int i = 0; i < L1.size(); i++)
+    //        L2.insert(2 * i, L1.get(i));
+    //    print(L2);
+    //
+    //    for (int i = 0; i < L1.size(); i++)
+    //        L2.set(i, L1.get(i));
+    //    print(L2);
+    //
+    //    int len = L2.size();
+    //    for (int i = 0; i < len; i++)
+    //        L2.remove(i);
+    //    print(L2);
 
 
-// Lists in the C++ Library
+    // Lists in the C++ Library
 
-//#include <vector>
-//    std::vector
+    //#include <vector>
+    //    std::vector
 
-//#include <list>
-//    std::list
+    //#include <list>
+    //    std::list
 
-// What are some of the operations supported by std::vector?
+    // What are some of the operations supported by std::vector?
 
-//    size_t size() const;
-//    void push_back(const ItemType &item);  // append
-//    iterator erase(iterator index);
-//    iterator insert(iterator index, const ItemType &item);
-//    ItemType &at(int index);
-//    ItemType &operator[](int index);
+    //    size_t size() const;
+    //    void push_back(const ItemType &item);  // append
+    //    iterator erase(iterator index);
+    //    iterator insert(iterator index, const ItemType &item);
+    //    ItemType &at(int index);
+    //    ItemType &operator[](int index);
 
-// DEMO	(write reverse method using indexes)
+    // DEMO	(write reverse method using indexes)
 
 
     vector<string> names;
@@ -122,68 +122,68 @@ int main() {
     names.emplace_back("Mark");
     names.emplace_back("John");
 
-//    print(names);
-//
-//    print(reverse(names));
+    //    print(names);
+    //
+    //    print(reverse(names));
 
 
-// What are some of the operations supported by std::list?
+    // What are some of the operations supported by std::list?
 
-// 	size_t size() const;
-// 	void push_back ( const ItemType& item );
-// 	iterator erase ( iterator index );
-// 	iterator insert ( iterator index, const ItemType& item );
-
-
-// DEMO	(change vector to list)
-
-// Iterators
-
-// How do you iterate over an array?
-
-//    for (int i = 0; i < length; i++)
-//        print(array[i]);
+    // 	size_t size() const;
+    // 	void push_back ( const ItemType& item );
+    // 	iterator erase ( iterator index );
+    // 	iterator insert ( iterator index, const ItemType& item );
 
 
-// How do you iterate over a list?
-// Why do you need an Iterator?
-// Why can't you use indexing?
+    // DEMO	(change vector to list)
 
-// 	some containers don't support indexing
-// 	internal structure can't be indexed efficiently
+    // Iterators
 
-// What's an Iterator?
+    // How do you iterate over an array?
 
-// 	a pointer that steps through the objects in a container
-
-// What operations are supported by an Iterator?
-// What's the abstract interface for an Iterator?
-
-// 	create();
-// 	access();
-// 	increment();
-// 	compare();
+    //    for (int i = 0; i < length; i++)
+    //        print(array[i]);
 
 
-// How do you create and use an Iterator on a list?
+    // How do you iterate over a list?
+    // Why do you need an Iterator?
+    // Why can't you use indexing?
 
-// 	  list<int> items;
-//    list<int>::iterator i = items.begin();
-//    while (i != items.end()) {
-//        cout << *i;
-//        i++;
-//    }
+    // 	some containers don't support indexing
+    // 	internal structure can't be indexed efficiently
+
+    // What's an Iterator?
+
+    // 	a pointer that steps through the objects in a container
+
+    // What operations are supported by an Iterator?
+    // What's the abstract interface for an Iterator?
+
+    // 	create();
+    // 	access();
+    // 	increment();
+    // 	compare();
 
 
-// DEMO	(rewrite reverse using iterators)
+    // How do you create and use an Iterator on a list?
+
+    // 	  list<int> items;
+    //    list<int>::iterator i = items.begin();
+    //    while (i != items.end()) {
+    //        cout << *i;
+    //        i++;
+    //    }
 
 
-// Why List?
+    // DEMO	(rewrite reverse using iterators)
 
-// A 'vector' can do most anything a 'list' can do.
-// A 'list' cannot do indexing, while a 'vector' can.
-// Why is a 'list' useful?
 
-// DEMO	(time for insert on vector vs list)
+    // Why List?
+
+    // A 'vector' can do most anything a 'list' can do.
+    // A 'list' cannot do indexing, while a 'vector' can.
+    // Why is a 'list' useful?
+
+    // DEMO	(time for insert on vector vs list)
 
 }
